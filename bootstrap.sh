f9fbf5b64f3e44b62ef845f45420b3a949bbc695
tetsuwo <tetsuwo.oishi@gmail.com>
2016-01-21 10:14:56 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,24 @@ HOME=$(pwd)
 
 echo "HOME directory"
 echo $HOME
+
 echo "Linking .hgrc"
 ln -snf $HOME/dotfiles/.hgrc $HOME/.hgrc
+echo "Linked"
+
 echo "Linking .vimrc"
 ln -snf $HOME/dotfiles/.vimrc $HOME/.vimrc
+echo "Linked"
+
 echo "Linking .gitconfig"
 ln -snf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-echo "Linking .vim directory"
-ln -snf $HOME/dotfiles/.vim/ $HOME/
+echo "Linked"
+
+#echo "Linking .vim directory"
+#ln -snf $HOME/dotfiles/.vim/ $HOME/
+
+echo "Installing NeoBundle ..."
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > $HOME/install.sh
+sh $HOME/install.sh
+rm $HOME/install.sh
+echo "Installed!"
